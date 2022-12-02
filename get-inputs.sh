c022fc583fdab89290cb0058721faf1d94f819fc
Paul Vaughan <paulieboo@gmail.com>
2022-12-02 23:16:38 +0000

    Script to get inputs for all days for all years. Don't abuse it: that sleep 5 is there for a reason.

---
+++ get-inputs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+COOKIE=your-aoc-session-cookie-here
+
+for YEAR in {2015..2022}; do
+    for DAY in {1..25}; do
+        echo "Downloading $YEAR day $DAY:"
+        curl https://adventofcode.com/$YEAR/day/$DAY/input --progress-bar --cookie "session=$COOKIE" -o inputs/$YEAR/$(printf "%02d" $DAY).txt
+        sleep 5
+        echo
+    done
+done
