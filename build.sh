a3b6266ff990c4caff5255d1a6c45cbc0f9c2cdb
Paul Vaughan <paulieboo@gmail.com>
2023-11-04 10:06:41 +0000

    Build script.

--- build.sh
+++ build.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-echo -e "\e[1mBuilding Linux...\e[0m"
-env GOOS=linux GOARCH=amd64 go build -trimpath -ldflags "-s -w" -a -o bin/ .
+THISYEAR=$(date +%Y)
+STARTYEAR=2015
+# STARTYEAR="${THISYEAR}"
 
-echo -e "\e[1mBuilding Windows...\e[0m"
-env GOOS=windows GOARCH=amd64 go build -trimpath -ldflags "-s -w" -a -o bin/ .
+for (( YEAR="${STARTYEAR}"; YEAR<="${THISYEAR}"; YEAR++ )); do
+    echo -e "\e[1mBuilding Linux ${YEAR}...\e[0m"
+    env GOOS=linux GOARCH=amd64 go build -trimpath -ldflags "-s -w" -a -o "bin/aoc-${YEAR}" "./cmd/${YEAR}"
 
-echo -e "\e[1mDone.\n\e[0m"
-ls -hl bin/
+    echo -e "\e[1mBuilding Windows ${YEAR}...\e[0m"
+    env GOOS=windows GOARCH=amd64 go build -trimpath -ldflags "-s -w" -a -o "bin/aoc-${YEAR}.exe" "./cmd/${YEAR}"
+
+    echo -e "\e[1mDone ${YEAR}.\e[0m\n"
+done
+
+echo -e "\e[1mAll done.\e[0m\n"
+sleep 1
+ls -ghl bin/
 
 echo
 file bin/*
