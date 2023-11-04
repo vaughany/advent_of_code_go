49cd026bb50d73f4ef3a103ec675996dfadaa33d
Paul Vaughan <paulieboo@gmail.com>
2023-11-04 09:33:23 +0000

    Puzzle input getter script update.

--- get-inputs.sh
+++ get-inputs.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 COOKIE=your-aoc-session-cookie-here
+WHOAMI=$(whoami)
+UNAME=$(uname -n)
 
-for YEAR in {2015..2022}; do
+for YEAR in {2015..2023}; do
     for DAY in {1..25}; do
-        echo "Downloading $YEAR day $DAY:"
-        curl -A "$(whoami)-$(uname -n) via curl" https://adventofcode.com/$YEAR/day/$DAY/input --progress-bar --cookie "session=$COOKIE" -o inputs/$YEAR/$(printf "%02d" $DAY).txt
+        echo "Downloading ${YEAR} day ${DAY}:"
+        OUTPUT="cmd/${YEAR}/inputs/$(printf "%02d" "${DAY}").txt"
+        curl -A "${WHOAMI}-${UNAME} via curl" "https://adventofcode.com/${YEAR}/day/${DAY}/input" --progress-bar --cookie "session=${COOKIE}" -o "${OUTPUT}"
         sleep 5
         echo
     done
