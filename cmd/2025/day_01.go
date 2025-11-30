9906ee1b4d51499ea0129ae39c0f0d3b141baa79
Paul Vaughan <paulieboo@gmail.com>
2025-11-30 21:33:39 +0000

    Use a more modern, supported Go version.

---
+++ cmd/2025/day_01.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"time"
+
+	"vaughany.com/advent_of_code_go/internal/loaders"
+	"vaughany.com/advent_of_code_go/internal/output"
+)
+
+func (cfg *config) day01(loader loaders.Loader) error {
+	timingStart := time.Now()
+
+	// 'instructions' can vary in type, depending on if we're dealing with ints, strings, bytes etc.
+	instructions, err := loaders.GetStrings(loader)
+	if err != nil {
+		return err
+	}
+	if cfg.timing {
+		output.TimeInfo(output.InfoTypeSetup, time.Since(timingStart))
+	}
+
+	timingPartOne := time.Now()
+	output.AnswerPart1(cfg.day01part1(instructions))
+	if cfg.timing {
+		output.TimeInfo(output.InfoTypeOne, time.Since(timingPartOne))
+	}
+
+	timingPartTwo := time.Now()
+	output.AnswerPart2(cfg.day01part2(instructions))
+	if cfg.timing {
+		output.TimeInfo(output.InfoTypeTwo, time.Since(timingPartTwo))
+	}
+
+	if cfg.timing {
+		output.TimeInfo(output.InfoTypeBoth, time.Since(timingPartOne))
+		output.TimeInfo(output.InfoTypeEverything, time.Since(timingStart))
+	}
+
+	return nil
+}
+
+// 2025-01-1:
+func (cfg *config) day01part1(instructions []string) (total int) {
+	return
+}
+
+// 2025-01-2:
+func (cfg *config) day01part2(instructions []string) (total int) {
+	return
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module vaughany.com/advent_of_code_go
 
-go 1.21.3
+go 1.25.4
