a983a93ab07eaa855be31a0bd2a84b7d64d4fa86
Paul Vaughan <paulieboo@gmail.com>
2025-12-07 20:51:19 +0000

    AoC 2024, day 4 (updated).

--- cmd/2024/day_04.go
+++ cmd/2024/day_04.go
@@ -13,18 +13,33 @@ func (cfg *config) day04() error {
 	)
 }
 
+var word = []byte("XMAS")
+
 // 2024-04-1: 2454
 func (cfg *config) day04part1(grid []string) (total int) {
-	maxX := len(grid[0])
 	maxY := len(grid)
+	maxX := len(grid[0])
 
-	for y := 0; y < maxY; y++ {
-		for x := 0; x < maxX; x++ {
-			if string(grid[y][x]) == `X` || string(grid[y][x]) == `S` {
-				total += day3Vertical(grid, x, y, maxY)
-				total += day3Horizontal(grid, x, y, maxX)
-				total += day3DiagonalUp(grid, x, y, maxX)
-				total += day3DiagonalDown(grid, x, y, maxX, maxY)
+	// Directions.
+	dirs := [][2]int{
+		{1, 0}, {-1, 0},
+		{0, 1}, {0, -1},
+		{1, 1}, {-1, -1},
+		{1, -1}, {-1, 1},
+	}
+
+	for y := range maxY {
+		row := []byte(grid[y])
+		for x := range maxX {
+			// Quick filter: only start from X.
+			if row[x] != 'X' {
+				continue
+			}
+
+			for _, d := range dirs {
+				if matchWord(grid, x, y, d[0], d[1]) {
+					total++
+				}
 			}
 		}
 	}
@@ -32,79 +47,66 @@ func (cfg *config) day04part1(grid []string) (total int) {
 	return
 }
 
-func day3Vertical(grid []string, x, y, maxY int) int {
-	if y+4 > maxY {
-		return 0
+func matchWord(grid []string, x, y, dx, dy int) bool {
+	maxY := len(grid)
+	maxX := len(grid[0])
+
+	for i := range word {
+		xx := x + dx*i
+		yy := y + dy*i
+
+		if xx < 0 || xx >= maxX || yy < 0 || yy >= maxY {
+			return false
+		}
+
+		if grid[yy][xx] != word[i] {
+			return false
+		}
 	}
 
-	guess := string(grid[y][x]) + string(grid[y+1][x]) + string(grid[y+2][x]) + string(grid[y+3][x])
-	if guess == `XMAS` || guess == `SAMX` {
-		return 1
-	}
-
-	return 0
-}
-
-func day3Horizontal(grid []string, x, y, maxX int) int {
-	if x+4 > maxX {
-		return 0
-	}
-
-	guess := string(grid[y][x]) + string(grid[y][x+1]) + string(grid[y][x+2]) + string(grid[y][x+3])
-	if guess == `XMAS` || guess == `SAMX` {
-		return 1
-	}
-
-	return 0
-}
-
-func day3DiagonalUp(grid []string, x, y, maxX int) int {
-	if x+4 > maxX || y-3 < 0 {
-		return 0
-	}
-
-	guess := string(grid[y][x]) + string(grid[y-1][x+1]) + string(grid[y-2][x+2]) + string(grid[y-3][x+3])
-	if guess == `XMAS` || guess == `SAMX` {
-		return 1
-	}
-
-	return 0
-}
-
-func day3DiagonalDown(grid []string, x, y, maxX, maxY int) int {
-	if x+4 > maxX || y+4 > maxY {
-		return 0
-	}
-
-	guess := string(grid[y][x]) + string(grid[y+1][x+1]) + string(grid[y+2][x+2]) + string(grid[y+3][x+3])
-	if guess == `XMAS` || guess == `SAMX` {
-		return 1
-	}
-
-	return 0
+	return true
 }
 
 // 2024-04-2: 1858
 func (cfg *config) day04part2(grid []string) (total int) {
-	maxX := len(grid[0])
 	maxY := len(grid)
+	maxX := len(grid[0])
 
 	for y := 0; y < maxY; y++ {
 		for x := 0; x < maxX; x++ {
-			total += day3X(grid, x, y, maxX, maxY)
+			total += day4XMAS(grid, x, y, maxX, maxY)
 		}
 	}
 
 	return
 }
 
-func day3X(grid []string, x, y, maxX, maxY int) int {
-	if x+3 > maxX || y+3 > maxY {
+func day4XMAS(grid []string, x, y, maxX, maxY int) int {
+	// We’re treating x,y as the top-left of a 3x3 block.
+	if x+2 >= maxX || y+2 >= maxY {
 		return 0
 	}
 
-	guess := string(grid[y][x]) + string(grid[y][x+2]) + string(grid[y+1][x+1]) + string(grid[y+2][x]) + string(grid[y+2][x+2])
-	if guess == `MMASS` || guess == `MSAMS` || guess == `SSAMM` || guess == `SMASM` {
+	// Center must be 'A'.
+	if grid[y+1][x+1] != 'A' {
+		return 0
+	}
+
+	// Diagonal 1: top-left to center to bottom-right.
+	d1a := grid[y][x]
+	d1b := grid[y+1][x+1]
+	d1c := grid[y+2][x+2]
+
+	// Diagonal 2: top-right to center to bottom-left.
+	d2a := grid[y][x+2]
+	d2b := grid[y+1][x+1]
+	d2c := grid[y+2][x]
+
+	diagIsMAS := func(a, b, c byte) bool {
+		return (a == 'M' && b == 'A' && c == 'S') || (a == 'S' && b == 'A' && c == 'M')
+	}
+
+	if diagIsMAS(d1a, d1b, d1c) && diagIsMAS(d2a, d2b, d2c) {
 		return 1
 	}
 
